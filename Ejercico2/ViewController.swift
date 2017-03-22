0967b291636a203b22b6a4bc0bcdfdba2c33bdcd
sberrueta15 <sberrueta15@gmail.com>
2017-03-21 22:02:18 -0300

    Initial Commit

---
+++ Ejercico2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ejercico2
+//
+//  Created by SP26 on 21/3/17.
+//  Copyright © 2017 UCUDAL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
